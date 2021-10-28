12e1463a10f6769cbcf69e7afa377f28d0dd2473
mvinyard <mvinyard@broadinstitute.org>
2021-10-28 04:08:52 +0000

    org into norm funcs

--- perturb_tools/_analysis/__init__.py
+++ perturb_tools/_analysis/__init__.py
@@ -1,4 +1,1 @@
 # analysis __init__.py
-
-
-from ._read_count_norm  import _log_normalize_read_count as lognorm
---
+++ perturb_tools/_normalization/__init__.py
@@ -0,0 +1,4 @@
+# analysis __init__.py
+
+
+from ._funcs._read_count_norm  import _log_normalize_read_count as lognorm
