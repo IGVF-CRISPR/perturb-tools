31df213fb5cc7eac4fe60fa9a99994d35134959d
Jayoung Kim Ryu <jayoung_ryu@g.harvard.edu>
2022-06-01 16:36:26 -0400

    Update setup.py
    relax python version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 setup(
     name="perturb-tools",
     version="0.0.11",
-    python_requires=">3.8.0",
+    python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
     url="https://github.com/mvinyard/perturb-tools",
