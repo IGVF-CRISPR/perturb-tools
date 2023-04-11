b3d144430b1466b6515e7df6d5a0ca0583d5b6ee
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2023-04-11 00:26:24 -0400

    removing untested modules

--- perturb_tools/_utilities/_funcs/_scan_sequence_for_motif.py
+++
@@ -1,7 +0,0 @@
-def _scan_sequence_for_motif(seq, motif, negative_report=False):
-    """Return location of motif if found"""
-
-    if motif in seq:
-        return seq.find(motif)
-    if negative_report == True:
-        return False
