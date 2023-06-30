64a17633a9149fe146df1f835b90116db3f216e1
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2023-06-30 14:11:33 -0400

    add pytest

--- tests/test_screen.py
+++ tests/test_screen.py
@@ -11,13 +11,13 @@ def screen():
         X=np.random.randint(low=0, high=1000, size=(4, 3)),
         dtype=int,
         var=pd.DataFrame(
-            np.random.rand(3, 1),
-            columns=[
-                "a",
-            ],
+            {"target": ["t1", "t1", "t2"]},
             index=["g1", "g2", "g3"],
         ),
-        obs=pd.DataFrame(index=["c1", "c2", "c3", "c4"]),
+        obs=pd.DataFrame(
+            {"replicate": [1, 2, 1, 2], "condition": [0, 0, 1, 1]},
+            index=["c1", "c2", "c3", "c4"],
+        ),
     )
 
 
@@ -32,4 +32,19 @@ class TestClass:
         assert screen_sel.shape == (2, 3)
         assert len(screen_sel.obs) == 2
 
-    
+    def test_lognorm(self, screen):
+        pt.pp.log_norm(screen)
+
+    def test_lfcs(self, screen):
+        pt.pp.log_norm(screen)
+        pt.pp.log_fold_change(screen, "c1", "c3")
+
+    def test_lfc_reps(self, screen):
+        pt.pp.log_norm(screen)
+        pt.pp.log_fold_change_reps(screen, 1, 0, compare_col="condition")
+        pt.pp.log_fold_change_aggregate(screen, 1, 0, compare_col="condition")
+
+    def test_write(self, screen):
+        pt.pp.log_norm(screen)
+        pt.io.to_Excel(screen, "test.xlsx")
+        pt.io.to_mageck_input(screen, "test.txt", target_column="target")
